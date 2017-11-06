65e4ef81993436c343cadadf314c7fc50087c323
Alex S <ags12432@gmail.com>
2017-11-05 22:33:55 -0500

    Added data persistence and set up the profile logic

--- Spark_2/src/spark_2/UserProfileUICntl.java
+++ Spark_2/src/spark_2/UserProfileUICntl.java
@@ -5,13 +5,20 @@
  */
 package spark_2;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  * FXML Controller class
@@ -29,6 +36,9 @@ public class UserProfileUICntl implements Initializable {
     private UserProfileEditCntl theUserProfileEditCntl;
     
     @FXML private Button editButton;
+    @FXML private Label nameLabel;
+    @FXML private Label personalityLabel;
+    @FXML private Label educationLabel;
     
     
     
@@ -42,9 +52,57 @@ public class UserProfileUICntl implements Initializable {
         
     }
     
+    public void showProfileData() throws ParseException {
+        
+        JSONParser parser = new JSONParser();
+        JSONObject data = new JSONObject();
+        
+        try {
+            
+            Object obj = parser.parse(new FileReader("data.json"));
+            data = (JSONObject) obj;
+            JSONArray userList = (JSONArray) data.get("users");
+            
+            for (Object user : userList) {
+                // get the overall user data (general user object)
+                JSONObject userJSON = (JSONObject) user;
+                
+                if(userJSON.get("Username").equals(Session.userID)) {
+                
+                    // grab the current user's profile data
+                    JSONArray userProfile = (JSONArray) userJSON.get("Profile Info");
+                    JSONObject name = (JSONObject) userProfile.get(0);
+                    JSONObject personality = (JSONObject) userProfile.get(1);
+                    JSONObject education = (JSONObject) userProfile.get(2);
+
+                    nameLabel.setText(name.get("Name").toString());
+                    personalityLabel.setText(personality.get("Personality").toString());
+                    educationLabel.setText(education.get("Education").toString());
+                    
+                    System.out.println(userJSON.get("Username"));
+                    System.out.println(name.get("Name"));
+                    System.out.println(personality.get("Personality"));
+                    System.out.println(education.get("Education"));
+                    
+                }
+            }
+            
+        }
+        
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        try {
+            showProfileData();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }    
     
 }
