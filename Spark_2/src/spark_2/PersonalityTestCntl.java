fa38131f7b9843b1d1f20f5e598c210ffa5de890
alex-samaha <ags12432@gmail.com>
2017-11-16 18:26:52 -0500

    Created PersonalityTestCntl
    Links the Personality Test button to the Personality Test UI

---
+++ Spark_2/src/spark_2/PersonalityTestCntl.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spark_2;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ags12
+ */
+public class PersonalityTestCntl {
+    
+    private Stage mainStage;
+    
+    
+    public PersonalityTestCntl(Stage currentStage) {
+        
+        // load in the stage and set up the scene
+        this.mainStage = currentStage;
+        setupPersonalityTestScene();
+        
+    }
+    
+    public void setupPersonalityTestScene() {
+        
+        
+        Parent root;
+        Scene scene;
+        
+        try {
+            // load the new fxml file
+            root = FXMLLoader.load(getClass().getResource("PersonalityTest.fxml"));
+            scene = new Scene(root, 600, 400);
+            scene.getStylesheets().add(getClass().getResource("PersonalityTest.css").toExternalForm());
+            
+            // set the stage with the new scene (Main Menu)
+            mainStage.setScene(scene);
+            mainStage.setTitle("Personality Test");
+            mainStage.show();
+        }
+        
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+}
