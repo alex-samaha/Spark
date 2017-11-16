1a5e7537df404a3f8656f9209d6426f5c868bf54
alex-samaha <ags12432@gmail.com>
2017-11-16 18:32:33 -0500

    Added Personality Test button functionality

--- Spark_2/src/spark_2/NavigationUICntl.java
+++ Spark_2/src/spark_2/NavigationUICntl.java
@@ -21,15 +21,28 @@ public class NavigationUICntl implements Initializable {
     
     private Stage mainStage;
     private UserProfileCntl theUserProfileCntl;
+    private PersonalityTestCntl thePersonalityTestCntl;
     
     @FXML private Button profileButton;
-    
+    @FXML private Button personalityTestButton;
     
     @FXML public void loadProfile(ActionEvent even) {
         
+        // grab the current window
         mainStage = (Stage) profileButton.getScene().getWindow();
         
+        // Instantiate UserProfileCntl and pass stage to load profile UI
         theUserProfileCntl = new UserProfileCntl(mainStage);
+        
+    }
+    
+    @FXML public void loadPersonalityTest(ActionEvent event) {
+        
+        // grab the current window
+        mainStage = (Stage) personalityTestButton.getScene().getWindow();
+        
+        // Instantiate PersonalityTestCntl and pass stage to load PersonalityTest UI
+        thePersonalityTestCntl = new PersonalityTestCntl(mainStage);
         
     }
     
