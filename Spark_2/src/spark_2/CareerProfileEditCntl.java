14f77049c3ee7529ad705787aefea0f57c48e9e2
alex-samaha <ags12432@gmail.com>
2017-11-17 22:13:41 -0500

    Add files via upload

---
+++ Spark_2/src/spark_2/CareerProfileEditCntl.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spark_2;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+/**
+ *
+ * @author Scott
+ */
+public class CareerProfileEditCntl {
+    private Stage mainStage;
+    
+    public CareerProfileEditCntl(Stage currentStage){
+        mainStage = currentStage;
+        setupScene();
+    }
+    
+    public void setupScene() {
+        
+        Parent root;
+        Scene scene;
+        
+        try {
+            // load the new fxml file
+            root = FXMLLoader.load(getClass().getResource("CareerProfileEditUI.fxml"));
+            scene = new Scene(root, 600, 400);
+            
+            // set the stage with the new scene (Main Menu)
+            mainStage.setScene(scene);
+            mainStage.setTitle("Career Profile Edit");
+            mainStage.show();
+        }
+        
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
